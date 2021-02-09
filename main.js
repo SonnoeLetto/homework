7371c62ffb2d1c92c13326e5637c536741175db9
SonnoeLetto <61381956+SonnoeLetto@users.noreply.github.com>
2021-02-09 15:06:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,50 +2,60 @@ function Student(name, surname, bdayYear) {
      this.name = name,
           this.surname = surname,
           this.bdayYear = bdayYear,
-          this.attendance = [],
-          this.rating = []
+          this.attendance = new Array(3),
+          this.rating = new Array(3)
 }
 
-Student.prototype.present = function () {
-     if (this.attendance.length < 30) {
+Student.prototype = {
+     get absent() {
+          const findVal = this.attendance.findIndex((el) => el === undefined)
+          if (findVal === -1) {
+               return console.log('Массив переполнен')
+          } else {
 
-          return this.attendance = [{ status: true }, ...this.attendance]
+               return this.attendance[findVal] = false;
+          }
+     },
+     get present() {
+          const findVal = this.attendance.findIndex((el) => el === undefined)
+          if (findVal === -1) {
+               return console.log('Массив переполнен')
+          } else {
+               return this.attendance[findVal] = true;
+          }
+     },
+
+     mark(number) {
+          const findVal = this.rating.findIndex((el) => el === undefined)
+
+          if (findVal === -1) {
+               return console.log('Массив переполнен')
+          } else {
+               return this.rating[findVal] = number
+          }
+     },
+     get summary() {
+          const sum = this.rating.reduce(function (acc, el) {
+
+               return acc += el
+          }, 0)
+          let midValue = sum / this.rating.length;
+
+
+          const ourpresent = this.attendance.filter(function (el) {
+               return el === true
+          }, 0)
+          const ratingStudent = ourpresent.length / this.attendance.length
+
+          if ((midValue >= 9) && (ratingStudent >= 0.9)) {
+               return alert('Ути какой молодчинка!')
+          } else if ((midValue <= 9) && (ratingStudent <= 0.9)) {
+               return alert("Редиска!")
+          } else {
+               return alert('Норм, но можно лучше')
+          }
+
      }
-};
-Student.prototype.absent = function () {
-     if (this.attendance.length < 30) {
-
-          return this.attendance = [{ status: false }, ...this.attendance]
-     }
-};
-Student.prototype.mark = function (number) {
-     if (this.rating.length < 30 && number > 0 && number <= 10) {
-          return this.rating = [{ yourRating: number }, ...this.rating]
-     }
-};
-Student.prototype.summary = function () {
-     const sum = this.rating.reduce(function (acc, el) {
-
-          return acc += el.yourRating
-     }, 0)
-     let midValue = sum / this.rating.length;
-
-
-     const ourpresent = this.attendance.filter(function (el) {
-          return el.status === true
-     }, 0)
-     const ratingStudent = ourpresent.length / this.attendance.length
-
-     if ((midValue >= 9) && (ratingStudent >= 0.9)) {
-          return alert('Ути какой молодчинка!')
-     } else if ((midValue <= 9) || (ratingStudent <= 0.9)) {
-          return alert('Норм, но можно лучше')
-     } else {
-          alert("Редиска!")
-     }
-
-};
-
-
+}
 const serg = new Student('serg', 'petrov', 1977);
 console.log(serg)
